9fa08168db5f12b485c73f26ee798569177c400f
Lucie Yarish <lucieyarish@hotmail.com>
2024-01-09 06:44:23 +0100

    Create nav bar; add basic styles

--- js/index.js
+++ js/index.js
@@ -1,11 +1,22 @@
 const postsContainer = document.getElementById('posts-container');
+const navBar = document.getElementById('nav-bar');
+
+const renderNavBar = () => {
+  const html = `
+        <h1 class="title">BlogSpace</h1>
+    `;
+
+  navBar.innerHTML = html;
+};
+
+renderNavBar();
 
 const renderPosts = (posts) => {
   const html = posts
     .map((post) => {
       return `
-        <h2>${post.title}</h2>
-        <p>${post.body}</p>
+        <h2 class="post-title">${post.title}</h2>
+        <p class="post-text">${post.body}</p>
     `;
     })
     .join('');
